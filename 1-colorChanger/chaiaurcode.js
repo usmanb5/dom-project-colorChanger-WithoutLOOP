ecc69489e73f40bbef23c9590b8c874cac11296f
usmanb5 <112270434+usmanb5@users.noreply.github.com>
2023-07-07 12:54:34 -0500

    Initial commit

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,28 @@
+let grey = document.getElementById("grey")
+grey.addEventListener('click', function(e){
+  console.log(e)
+  console.log(e.target)
+  document.querySelector("body").style.backgroundColor = "grey"
+
+})
+let white = document.getElementById("white")
+white.addEventListener('click', function(e){
+  console.log(e)
+  console.log(e.target)
+  document.querySelector("body").style.backgroundColor = "white"
+
+})
+let blue = document.getElementById("blue")
+blue.addEventListener('click', function(e){
+  console.log(e)
+  console.log(e.target)
+  document.querySelector("body").style.backgroundColor = "blue"
+
+})
+let yellow = document.getElementById("yellow")
+yellow.addEventListener('click', function(e){
+  console.log(e)
+  console.log(e.target)
+  document.querySelector("body").style.backgroundColor = "yellow"
+
+})
---
+++ README.md
@@ -0,0 +1,3 @@
+# dom-project-colorChanger-WithoutLOOP
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dom-project-chaiaurcode-kpxe9t)
